820d14760d4b2aeb5e597e634d783dbefebc1f0e
Sawkez <sav.chirkow2015@gmail.com>
2026-08-21 20:19:09 +0400

    fix unable to select existing project

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -5,7 +5,7 @@ func _on_project_directory_selection_dir_selected(path: String) -> void:
 	var is_empty := DirAccess.get_files_at(path).is_empty() and DirAccess.get_directories_at(path).is_empty()
 	var is_project := FileAccess.file_exists(path + "/mod.json")
 	
-	if is_empty and is_project:
+	if is_project and not is_empty:
 		Global.project_path = path
 		get_tree().change_scene_to_file("res://scenes/project/project_menu.tscn")
 	
